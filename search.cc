40b2127458358069ce812ebed77dcdde28f50e13
pelliott <pelliott@be659556-b4d2-0310-98d5-f0e407598ecd>
2004-03-17 17:55:50 +0000

    Finish Gtk part of Search Dialog.

--- search.cc
+++ search.cc
@@ -4,14 +4,34 @@
 
 namespace SearchTextView {  // avoid namespace conficts.
 
-  SearchCenter::SearchCenter(Gtk::Window& parent): Gtk::Dialog(
-					    _("Regular Expression Search"),
-					    parent,
-					    true,
-					    true
-					    )
+  SearchCenter::SearchCenter(Gtk::Window& parent): 
+    Gtk::Dialog(
+		_("Regular Expression Search"),
+		parent,
+		true,
+		true
+		),
+
+    // init shown by glade
+    regex_vbox(false,0),
+    top_hbox(false,0),
+    entry_frame(_("Regular Expression search string")),
+    regex_entry(),
+    type_frame(_( "Regular Expression type" )),
+    checkbox_vbox(false,0),
+    type_vbox(false,0),
+    type_group(),
+    literal_radiobutton(type_group,_("literal")),
+    normal_radiobutton(type_group,_("normal")),
+    basic_radiobutton(type_group,_("basic")),
+    extended_radiobutton(type_group,_("extended")),
+
+    exact_checkbutton(_("require exact case match")),
+    reverse_checkbutton(_("reverse search direction"))
+    
   {
 
+    // most of this shown by glade-2.
     add_button(Gtk::StockID("gtk-cancel"), Gtk::RESPONSE_CANCEL );
     add_button(Gtk::StockID("gtk-ok"),   Gtk::RESPONSE_OK );
 
@@ -19,6 +39,85 @@ namespace SearchTextView {  // avoid namespace conficts.
     set_response_sensitive(Gtk::RESPONSE_CANCEL);
     set_response_sensitive(Gtk::RESPONSE_OK);
     set_default_response(Gtk::RESPONSE_CANCEL);
+
+    get_vbox()->pack_start(regex_vbox);
+    get_vbox()->set_homogeneous(false);
+    get_vbox()->set_spacing(0);
+
+    regex_vbox.pack_start(top_hbox);
+    regex_vbox.pack_start(entry_frame, Gtk::PACK_SHRINK, 0);
+
+    regex_entry.set_flags(Gtk::CAN_FOCUS);
+    regex_entry.set_visibility(true);
+    regex_entry.set_editable(true);
+    regex_entry.set_max_length(0);
+    regex_entry.set_text(_(""));
+    regex_entry.set_has_frame(true);
+    regex_entry.set_activates_default(false);
+
+    entry_frame.set_border_width(3);
+    entry_frame.set_shadow_type(Gtk::SHADOW_ETCHED_IN);
+    entry_frame.set_label_align(0,0.5);
+    entry_frame.add(regex_entry);
+
+    top_hbox.pack_start(type_frame);
+    top_hbox.pack_start(checkbox_vbox);
+
+    type_frame.set_shadow_type(Gtk::SHADOW_ETCHED_IN);
+    type_frame.set_label_align(0,0.5);
+    type_frame.add(type_vbox);
+
+    exact_checkbutton.set_flags(Gtk::CAN_FOCUS);
+    exact_checkbutton.set_relief(Gtk::RELIEF_NORMAL);
+    exact_checkbutton.set_mode(true);
+    exact_checkbutton.set_active(true);
+    reverse_checkbutton.set_flags(Gtk::CAN_FOCUS);
+    reverse_checkbutton.set_relief(Gtk::RELIEF_NORMAL);
+    reverse_checkbutton.set_mode(true);
+    reverse_checkbutton.set_active(false);
+
+    checkbox_vbox.pack_start(exact_checkbutton, Gtk::PACK_SHRINK, 0);
+    checkbox_vbox.pack_start(reverse_checkbutton, Gtk::PACK_SHRINK, 0);
+
+    literal_radiobutton.set_flags(Gtk::CAN_FOCUS);
+    literal_radiobutton.set_relief(Gtk::RELIEF_NORMAL);
+    literal_radiobutton.set_mode(true);
+    literal_radiobutton.set_active(false);
+    normal_radiobutton.set_flags(Gtk::CAN_FOCUS);
+    normal_radiobutton.set_relief(Gtk::RELIEF_NORMAL);
+    normal_radiobutton.set_mode(true);
+    normal_radiobutton.set_active(true);
+    basic_radiobutton.set_flags(Gtk::CAN_FOCUS);
+    basic_radiobutton.set_relief(Gtk::RELIEF_NORMAL);
+    basic_radiobutton.set_mode(true);
+    basic_radiobutton.set_active(false);
+    extended_radiobutton.set_flags(Gtk::CAN_FOCUS);
+    extended_radiobutton.set_relief(Gtk::RELIEF_NORMAL);
+    extended_radiobutton.set_mode(true);
+    extended_radiobutton.set_active(false);
+
+    type_vbox.pack_start(literal_radiobutton, Gtk::PACK_SHRINK, 0);
+    type_vbox.pack_start(normal_radiobutton, Gtk::PACK_SHRINK, 0);
+    type_vbox.pack_start(basic_radiobutton, Gtk::PACK_SHRINK, 0);
+    type_vbox.pack_start(extended_radiobutton, Gtk::PACK_SHRINK, 0);
+
+    exact_checkbutton.show();
+    reverse_checkbutton.show();
+
+    literal_radiobutton.show();
+    normal_radiobutton.show();
+    basic_radiobutton.show();
+    extended_radiobutton.show();
+
+    type_vbox.show();
+    type_frame.show();
+    checkbox_vbox.show();
+    top_hbox.show();
+    regex_entry.show();
+    entry_frame.show();
+    regex_vbox.show();
+
+    // most of the above came from glade-2
 
     // do not show dialog, make it appear on tasklist when not in use!
     // run() it instead.
--- search.hh
+++ search.hh
@@ -4,6 +4,10 @@
 #include "config.h"
 #include <gtkmm/textview.h>
 #include <gtkmm/dialog.h>
+#include <gtkmm/frame.h>
+#include <gtkmm/entry.h>
+#include <gtkmm/radiobutton.h>
+#include <gtkmm/checkbutton.h>
 
 
 #include <boost/regex.hpp>
@@ -26,6 +30,18 @@ private:
   SearchCenter();
   SearchCenter(const SearchCenter&);
   SearchCenter& operator=(const SearchCenter&);
+
+  Gtk::VBox regex_vbox;
+  Gtk::HBox top_hbox;
+  Gtk::Frame entry_frame;
+  Gtk::Entry regex_entry;
+  Gtk::Frame type_frame;
+  Gtk::VBox checkbox_vbox;
+  Gtk::VBox type_vbox;
+  Gtk::RadioButton_Helpers::Group type_group;
+  Gtk::RadioButton literal_radiobutton,normal_radiobutton,
+                   basic_radiobutton,extended_radiobutton;
+  Gtk::CheckButton exact_checkbutton,reverse_checkbutton;
 
 };
 
