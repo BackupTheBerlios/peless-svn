acfa21d609331bcac0e00ac436878e7df1da6dd1
pelliott <pelliott@be659556-b4d2-0310-98d5-f0e407598ecd>
2007-05-26 03:38:04 +0000

    fix copyright notices.

--- forGtkmm2-2.4/inittime.hh
+++ forGtkmm2-2.4/inittime.hh
@@ -1,21 +1,24 @@
 //
 //     PELESS TABBED TEXT FILE LISTER.
-//     Copyright (C) 2004 Paul Elliott
+//     Copyright (C) 2004,2007 Paul Elliott
 //     
-//     This program is free software; you can redistribute it and/or modify
+//     This file is part of peless.
+// 
+//     peless is free software; you can redistribute it and/or modify
 //     it under the terms of the GNU General Public License as published by
-//     the Free Software Foundation; version 1.
-//     
-//     This program is distributed in the hope that it will be useful,
+//     the Free Software Foundation; either version 2 of the License, or
+//     (at your option) any later version.
+// 
+// 
+//     peless is distributed in the hope that it will be useful,
 //     but WITHOUT ANY WARRANTY; without even the implied warranty of
 //     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 //     GNU General Public License for more details.
-//     
+// 
+// 
 //     You should have received a copy of the GNU General Public License
-//     along with this program; if not, write to the Free Software
-//     Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
-//
-//     Paul Elliott pelliott@io.com
+//     along with peless; if not, write to the Free Software
+//     Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 #ifndef _INITTIME_
 #define _INITTIME_ 1
 #include  <boost/utility.hpp>
